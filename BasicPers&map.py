1bc917518e2fe02499914e9c247c0a2ca2668ebc
Nikit-a <bgum71@gmail.com>
2016-12-16 15:51:29 +0300

    Update BasicPers&map.py

--- BasicPers&map.py
+++ BasicPers&map.py
@@ -1,49 +1,56 @@
 import pygame
 
+# Set display size
 d_width = 512;
 d_height = 256;
 
+# Initialization
 pygame.init();
 gameDisplay = pygame.display.set_mode((d_width,d_height));
+# Window title
 pygame.display.set_caption('Action RPG');
+# Needed to correctly set fps
 clock = pygame.time.Clock()
 
-##persImg = pygame.image.load('./Pers.png');
-##mapImg = pygame.image.load('./Map.png');
+# Loading images from directory
 persImg = pygame.image.load('./IsometricPers.png');
 mapImg = pygame.image.load('./IsometricMap.png');
 
-
+# Function that shows users pers on display
 def Pers(x,y):
     gameDisplay.blit(persImg, (x,y))
 ##    print 'Pers position: ',x,':',y
 
+# Shows image under pers
 def Map(x,y):
     gameDisplay.blit(mapImg, (x,y))
 
 def jump():
     pass
 
+# Flag to end running application
 crashed = False
 
-##xMap = d_width/2 - 50;
-##yMap = d_height/2 - 50;
+# Map image location
 xMap = 0;
 yMap = 0;
 
+# Can be used as pers speed
 xPers = d_width/2;
 yPers = d_height/2;
 
+# Bias for statment when key is not pressed
 x_bias = 0;
 y_bias = 0;
 
+# Application runs while flag 'crashed' is set to False 
 while not crashed:
-
+    # Cycle catches all events
     for event in pygame.event.get():
 
         if event.type == pygame.QUIT:
             crashed = True
-    
+    # Catches all pressed keys
     if event.type == pygame.KEYDOWN:
 
         if event.key == pygame.K_LEFT:
@@ -58,10 +65,11 @@ while not crashed:
         if event.key == pygame.K_DOWN:
             y_bias = 2
 
-
+    # Pers moves
     xPers += x_bias;
     yPers += y_bias;
 
+    # Fill display with black color and put Map and Pers image on it
     gameDisplay.fill((0,0,0))
     Map(xMap,yMap)
     Pers(xPers,yPers)                
